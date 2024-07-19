d91ff4f2455a1798e60c5deb0012cbaf8ab58735
toxeeec <bartosz.kapciak@gmail.com>
2024-07-19 20:24:27 +0200

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...eslintPluginAstro.configs.recommended,
+	...eslintPluginAstro.configs["jsx-a11y-strict"],
+	{
+		languageOptions: {
+			parserOptions: {
+				project: true,
+				tsconfigRootDir: import.meta.dir,
+			},
+		},
+	},
+);
