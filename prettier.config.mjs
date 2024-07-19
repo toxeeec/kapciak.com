fda96999a17dd5499f10fbb1ec959cad7222e3fe
toxeeec <bartosz.kapciak@gmail.com>
2024-07-19 16:05:09 +0200

    feat: add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+	plugins: ["prettier-plugin-astro"],
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+};
