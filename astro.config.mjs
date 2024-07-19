29b4ea1b6bb911af3503aa4727f9cff6075d132a
toxeeec <bartosz.kapciak@gmail.com>
2024-07-19 21:30:01 +0200

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), sitemap()],
 	site: "https://bartoszkapciak.com",
 });
